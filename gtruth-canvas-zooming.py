4a39f076535696a6560f9397a7f7a382a5e81736
mondaugen <n.alois.esterer@gmail.com>
2013-03-21 19:58:12 -0400

    moved helpmess to gtruthhelp.py and made rectangle border widths constant regardless of zoom level

--- gtruth-canvas-zooming.py
+++ gtruth-canvas-zooming.py
@@ -14,69 +14,13 @@ from gamera import classify
 from gamera import knn
 from gtruthrect import *
 from gtruthtextedit import *
-from gtruthhelp import GtruthHelpFrame
+from gtruthhelp import GtruthHelpFrame, helpmess
 
 # For loading meifiles
 from pymei import MeiDocument, MeiElement, XmlImport
 
-''' Save to mei file instead of text file. Must append path to meicreate.py to
-PYTHONPATH environment variable unless this is run in the same directory as it. '''
-
-helpmess = \
-'''\
-To open a picture to draw boxes upon, go to
-File->Open
-and select the image from the file dialog.
-
-To save the boxes that you have drawn onto the picture go to 
-File->Save
-and then choose a file path and a filename in the file \
-dialog (a filename will be suggested to you).
-
-To draw a box onto the picture, click and hold the left mouse \
-button. Release the button when you have drawn a box of the size of \
-your liking.
-
-To delete a box already drawn, hold down control and click on the \
-box you would like to delete. If multiple boxes are beneath the \
-cursor, the smallest will be deleted.
-
-To resize a box that has already been drawn, hold down shift and \
-click and hold the left mouse button.  Release the button once you \
-have resized the box to your liking. Note that you may only change \
-the position of the bottom right hand corner, to change any other \
-corners, the box must be deleted and redrawn.
-
-There are two kinds of boxes that can be drawn: there are boxes \
-that bound the bars and boxes that bound the staves. The former \
-are in ren and the latter in green. To toggle between the two \
-types, go to
-File->(Bar mode | Staff mode)
-depending on what mode you are in currently. Note that when \
-deleting boxes you will only delete the type of boxes whose mode \
-you are in currently. This is also true for the Clear method.
-
-There is a minimum box size that you are allowed to draw to keep \
-you from saving some erroneous boxes. If you are finding that it \
-be too small or large, it may be adjusted using Increase Minimum \
-and Decrease Minimum.
-
-You may get a warning message upon saving that some measures have \
-not been numbered. This is most likely because a measure is not \
-inside a staff bounding box. You have the option of going back \
-and ensuring all measure boxes are enclosed in staff boxes, which \
-you must do to save an accurate MEI file in the end.
-
-The text box is available for making annotations. Upon saving, its \
-contents will be saved to the file-path specified for the MEI file \
-but ending in .txt.
-
-WARNING: If you scroll while drawing a box it will mess up the top \
-corner coordinates. If you would like to draw a box larger than the \
-screen, simply draw the box as large as you can, let go of the \
-mouse button, scroll the window and then shift-click on the box to \
-resize it.
-'''
+'''Must append path to meicreate.py to PYTHONPATH environment variable unless
+this is run in the same directory as it. '''
 
 # for debugging
 __GTRUTH_DEBUG__ = True
@@ -202,13 +146,13 @@ class MainWindow(wx.ScrolledWindow):
             dc.SetBrush(wx.Brush('WHITE',\
                     style=wx.BRUSHSTYLE_TRANSPARENT))
             dc.SetPen(wx.Pen('RED',\
-                    width=3, style=wx.PENSTYLE_SOLID))
+                    width=3.0/self.userscale[0], style=wx.PENSTYLE_SOLID))
             dc.DrawRectangle(*p.GetBox())
         for p in self.staffpanels:
             dc.SetBrush(wx.Brush('WHITE',\
                     style=wx.BRUSHSTYLE_TRANSPARENT))
             dc.SetPen(wx.Pen('GREEN',\
-                    width=3, style=wx.PENSTYLE_SOLID))
+                    width=3.0/self.userscale[1], style=wx.PENSTYLE_SOLID))
             dc.DrawRectangle(*p.GetBox())
 
     def Zoom(self, factor):
@@ -388,7 +332,6 @@ class MyFrame(wx.Frame):
     def __init__(self, parent, title):
         wx.Frame.__init__(self, parent, wx.ID_ANY, title)
 
-        ### sw-canvas-funtions doesn't include rectmode yet
         # 'BAR' mode can be set to set the mouse to draw rectangles for the bars
         # 'STAFF' can be set to set the mouse to draw raectangels for the staves
         self.rectmode = 'BAR' # default start in 'BAR' mode
--- gtruthhelp.py
+++ gtruthhelp.py
@@ -2,6 +2,62 @@
 The text editor for barline ground-truth.
 '''
 
+helpmess = \
+'''\
+To open a picture to draw boxes upon, go to
+File->Open
+and select the image from the file dialog.
+
+To save the boxes that you have drawn onto the picture go to 
+File->Save
+and then choose a file path and a filename in the file \
+dialog (a filename will be suggested to you).
+
+To draw a box onto the picture, click and hold the left mouse \
+button. Release the button when you have drawn a box of the size of \
+your liking.
+
+To delete a box already drawn, hold down control and click on the \
+box you would like to delete. If multiple boxes are beneath the \
+cursor, the smallest will be deleted.
+
+To resize a box that has already been drawn, hold down shift and \
+click and hold the left mouse button.  Release the button once you \
+have resized the box to your liking. Note that you may only change \
+the position of the bottom right hand corner, to change any other \
+corners, the box must be deleted and redrawn.
+
+There are two kinds of boxes that can be drawn: there are boxes \
+that bound the bars and boxes that bound the staves. The former \
+are in ren and the latter in green. To toggle between the two \
+types, go to
+File->(Bar mode | Staff mode)
+depending on what mode you are in currently. Note that when \
+deleting boxes you will only delete the type of boxes whose mode \
+you are in currently. This is also true for the Clear method.
+
+There is a minimum box size that you are allowed to draw to keep \
+you from saving some erroneous boxes. If you are finding that it \
+be too small or large, it may be adjusted using Increase Minimum \
+and Decrease Minimum.
+
+You may get a warning message upon saving that some measures have \
+not been numbered. This is most likely because a measure is not \
+inside a staff bounding box. You have the option of going back \
+and ensuring all measure boxes are enclosed in staff boxes, which \
+you must do to save an accurate MEI file in the end.
+
+The text box is available for making annotations. Upon saving, its \
+contents will be saved to the file-path specified for the MEI file \
+but ending in .txt.
+
+WARNING: If you scroll while drawing a box it will mess up the top \
+corner coordinates. If you would like to draw a box larger than the \
+screen, simply draw the box as large as you can, let go of the \
+mouse button, scroll the window and then shift-click on the box to \
+resize it.
+'''
+
 import wx
 
 class GtruthHelpFrame(wx.Frame):
